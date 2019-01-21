af2b4f67553c7ef5cc7d3cab00899a3c4616ce62
xputnikx <xputnikx@gmail.com>
2019-01-21 22:45:48 +0100

    push project with readme and example

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# flutter-passcode
-Flutter - Passcode Lock 
+# Flutter - Passcode Lock Screen
+
+A Flutter plugin for iOS and Android for showing passcode input screen, similar to Native iOS.
+
+<img src="passcode_screen_example/passcode-screen-demo.gif" width="300">
+
+## Installation
+
+First add `passcode_screen` as a [dependency in your pubspec.yaml file](https://flutter.io/platform-plugins/).
+
+Then use import
+
+```dart
+import 'package:passcode_screen/passcode_screen.dart';
+```
+
+## What can it do for you?
+
+<img src="passcode_screen_example/passcode-screen-default.png" width="300">
+
+1. Create a beautiful passcode lock view simply.
+
+```dart
+PasscodeScreen(
+  title: title,
+  passwordEnteredCallback: _onPasscodeEntered,
+  cancelLocalizedText: 'Cancel',
+  deleteLocalizedText: 'Delete',
+  shouldTriggerVerification: _verificationNotifier.stream,  
+);
+```
+
+2. Passcode input completed callback.
+```dart
+_onPasscodeEntered(String enteredPasscode) {
+  
+}
+```
+
+3. Notify passcode screen if passcode correct or not 
+```dart
+final StreamController<bool> _verificationNotifier = StreamController<bool>.broadcast();
+
+_onPasscodeEntered(String enteredPasscode) {
+  bool isValid = '123456' == enteredPassword;
+  _verificationNotifier.add(isValid);
+}
+
+```
+
+Don't forget to close a stream
+```dart
+@override
+void dispose() {
+  _verificationNotifier.close();
+  super.dispose();
+}
+
+```
+
+4. Customize UI.
+
+Customize circles
+```dart
+class CircleUIConfig {
+  final Color borderColor;
+  final Color fillColor;
+  final double borderWidth;
+  final double circleSize;
+  double extraSize;
+}
+```
+
+Customize keyboard
+```dart
+class KeyboardUIConfig {
+  final double digitSize;
+  final TextStyle digitTextStyle;
+  final TextStyle deleteButtonTextStyle;
+  final Color primaryColor;
+  final Color digitFillColor;
+  final EdgeInsetsGeometry keyboardRowMargin;
+  final EdgeInsetsGeometry deleteButtonMargin;
+}
+```
+
+<img src="passcode_screen_example/passcode-screen-custom.png" width="300">
+
+### iOS & Android
+
+Plugin is totally platform agnostic. No configuration required - the plugin should work out of the box.
+
+### Example
+
+
+
+## Contributions
+Warmly welcome to submit a pull request!
+
+## Passcode Lock Screen
+Passcode Lock Screen is owned and maintained by [Redeyes Dev](http://redeyes-dev.com)
+
+### Used in production
+[Passkeep - Passwords Keeper](http://bit.ly/andpasskeep) 
+
+### Contributors 
+
+[Vladimir Hudnitsky](https://github.com/xPutnikx) 
+
---
+++ passcode_screen_example/lib/main.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:passcode_screen/circle.dart';
+import 'package:passcode_screen/keyboard.dart';
+import 'package:passcode_screen/passcode_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Passcode Lock Screen Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ExampleHomePage(title: 'Passcode Lock Screen Example'),
+    );
+  }
+}
+
+class ExampleHomePage extends StatefulWidget {
+  ExampleHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => _ExampleHomePageState();
+}
+
+class _ExampleHomePageState extends State<ExampleHomePage> {
+  final StreamController<bool> _verificationNotifier =
+      StreamController<bool>.broadcast();
+
+  bool isAuthenticated = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You are ${isAuthenticated ? '' : 'NOT'} authenticated'),
+            _defaultLockScreenButton(context),
+            _customColorsLockScreenButton(context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  _defaultLockScreenButton(BuildContext context) => MaterialButton(
+        color: Theme.of(context).primaryColor,
+        child: Text('Open Default Lock Screen'),
+        onPressed: () {
+          _showLockScreen(context, opaque: false);
+        },
+      );
+
+  _customColorsLockScreenButton(BuildContext context) => MaterialButton(
+        color: Theme.of(context).primaryColor,
+        child: Text('Open Custom Lock Screen'),
+        onPressed: () {
+          _showLockScreen(context,
+              opaque: false,
+              circleUIConfig: CircleUIConfig(
+                  borderColor: Colors.blue,
+                  fillColor: Colors.blue,
+                  circleSize: 30),
+              keyboardUIConfig: KeyboardUIConfig(digitBorderWidth: 2, primaryColor: Colors.blue));
+        },
+      );
+
+  _showLockScreen(BuildContext context,
+      {bool opaque,
+      CircleUIConfig circleUIConfig,
+      KeyboardUIConfig keyboardUIConfig}) {
+    Navigator.push(
+        context,
+        PageRouteBuilder(
+          opaque: opaque,
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              PasscodeScreen(
+                title: 'Enter App Passcode',
+                circleUIConfig: circleUIConfig,
+                keyboardUIConfig: keyboardUIConfig,
+                passwordEnteredCallback: _onPasscodeEntered,
+                cancelLocalizedText: 'Cancel',
+                deleteLocalizedText: 'Delete',
+                shouldTriggerVerification: _verificationNotifier.stream,
+              ),
+        ));
+  }
+
+  _onPasscodeEntered(String enteredPasscode) {
+    bool isValid = '123456' == enteredPasscode;
+    _verificationNotifier.add(isValid);
+    if (isValid) {
+      setState(() {
+        this.isAuthenticated = isValid;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _verificationNotifier.close();
+    super.dispose();
+  }
+}

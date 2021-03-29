c7911dfdb1f6d9903094e3a0092a8913a0ebc5f0
xputnikx <xputnikx@gmail.com>
2021-03-29 10:16:29 +0200

    refactor, bump version to 2.0.0

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Fork\flutter-passcode\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/vladimirhudnitsky/Developer/JAVA/BUSINESS/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/vladimirhudnitsky/Developer/JAVA/BUSINESS/SSPassword/Android/sspasswords/passcode_screen/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
